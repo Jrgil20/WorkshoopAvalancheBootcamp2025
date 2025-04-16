6906ef119bcda14334053ef97270c468caa25780
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2025-04-16 19:48:35 +0000

    Implementar la función de donación y corregir los tipos de retorno en las funciones de donantes

--- Arrays.sol
+++ Arrays.sol
@@ -8,7 +8,7 @@ contract Charity {
     // Función para registrarse como donante (omitimos el envio de dinero por ahora)
     function donar() public {
         // ¿Cómo hacemos para almacenar a la persona que interactuó con la función como donador?
-        donadores.push(???);
+        donadores.push(msg.sender);
     }
 
     /* 
@@ -16,7 +16,7 @@ contract Charity {
         ¿Es view o pure?
         ¿Que tipo de dato debe retornar para que nos indique el ultimo donador?
     */
-    function ultimoDonador() public ??? returns (???) {
+    function ultimoDonador() public view returns (address) {
         // Devuelve la última dirección del array
         return donadores[donadores.length - 1];
     }
@@ -26,8 +26,8 @@ contract Charity {
         ¿Es view o pure?
         ¿Que tipo de dato debe retornar para que nos indique la cantidad de donadores?
     */
-    function cantidadDonadores() public ??? returns (???) {
+    function cantidadDonadores() public view returns (uint256) {
         // Retorna la longitud del array
-        return ???.length;
+        return donadores.length;
     }
 }
