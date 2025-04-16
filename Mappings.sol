db5b07c104bdb1db6f590f83f1b938cbb3e906ff
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2025-04-16 19:54:34 +0000

    Corregir tipos de datos en los mappings y funciones del contrato Votacion

--- Mappings.sol
+++ Mappings.sol
@@ -6,13 +6,13 @@ contract Votacion {
         Mapping que permita almacenar votos por candidato
         ¿Que tipo de dato nos sirve para almacenar los votos?
     */
-    mapping (address => ???) public votos;
+    mapping (address => uint256) public votos;
 
     /* 
         Mapping que rastree si un usuario ya votó
         ¿Que tipo de dato nos sirve para saber si una persona votó?
     */
-    mapping (address => ???) public yaVoto;
+    mapping (address => bool) public yaVoto;
 
     // Función para votar por un candidato
     function votar(address candidato) public {
@@ -21,6 +21,7 @@ contract Votacion {
             Usamos la variable global msg.sender para almacenar la 
             dirección de la persona que ejecutó la función (el votante)
         */
+        require(!yaVoto[msg.sender], "Ya has votado anteriormente");
         yaVoto[msg.sender] = true;
         /* 
             Incrementa los votos del candidato
@@ -34,7 +35,7 @@ contract Votacion {
         ¿Es view o pure?
         ¿Que tipo de dato debe retornar para que nos indique si un usuario votó?
     */
-    function verificarSiVoto(address votante) public ??? returns (???) {
+    function verificarSiVoto(address votante) public view returns (bool) {
         return yaVoto[votante];
     }
 
@@ -44,7 +45,7 @@ contract Votacion {
         ¿Que tipo de dato debe retornar para que nos indique la cantidad de votos?
         ¿Cómo accedemos al mapping y que dato le pasamos para que retorne los votos del candidato?
     */
-    function obtenerVotos(address candidato) public ??? returns (???) {
-        return ???[???];
+    function obtenerVotos(address candidato) public view returns (uint256) {
+        return votos[candidato];
     }
 }
